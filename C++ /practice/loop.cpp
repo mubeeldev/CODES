9a1954cdd128c382636a8d33214b654ff4c95180
mubeeldev <muhammadmdgh1@gmail.com>
2026-05-12 05:50:08 +0300

    student managemrnt system

---
+++ C++ /practice/loop.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+#include <string>
+
+int main(){
+    // int numbers[5] = {10,20,30,40,50};
+    // for(int num :numbers){
+    //     cout << num << endl;
+    // }
+
+    // string word = "Hello";
+    // for(char c : word){
+    //     cout << c << endl;
+    // }
+
+    for( int i = 2; i <=512; i *= 2){
+        cout << i << endl;
+    }
+
+
+
+    return 0;
+}
---
+++ C++ /student management system/tempCodeRunnerFile.cpp
@@ -0,0 +1,3 @@
+  // for(string opration : oprations) {
+            //     cout << opration << " ";
+            // }
